ddb9725244737aef7e1378a87129a339455623d2
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-28 19:21:57 +0800

    [TodoList][Baron-JC]refactor: added page not found from antd

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,15 @@
+import { Button, Result } from 'antd';
 import React, { Component } from 'react';
 
 class PageNotFound extends Component {
   render() {
     return (
-      <div>
-        <h1>PAGE NOT FOUND!</h1>
-      </div>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button type="primary" href="/">Back Home</Button>}
+      />
     );
   }
 }
