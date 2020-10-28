f71fda837989536404e039e5877f4d541e69579a
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-28 21:39:47 +0800

    [TodoList][Baron-JC]feature: done list is now printed in antd Table

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import { List, Card } from 'antd';
+
+import { Table } from 'antd';
+
 class DoneList extends Component {
 
   render() {
-    const doneTodos = this.props.doneTodos.map((todo) =>
-      <Card key={todo.id}>{todo.text} </Card>);
+
+    const columns = [
+      {
+        title: 'Done',
+        dataIndex: 'text',
+        key: 'id',
+        render: text => <b>{text}</b>,
+      },
+      {
+        title: 'Date Created',
+        dataIndex: 'createdAt',
+        key: 'id',
+        render: text => <b>{text}</b>,
+      }
+    ];
+
     return (
       <div>
         <h1 id="appName">TodoList</h1>
-        <h2 id="appName">Done Items</h2>
-        <List
-          grid={{ gutter: 16, column: 4 }}
-          dataSource={doneTodos}
-          renderItem={item => (
-            <List.Item>
-              {item}
-            </List.Item>
-          )}
+        <Table 
+          columns={columns}
+          dataSource={this.props.doneTodos}
+          pagination={{ pageSize: 10 }}
         />
       </div>
     );
