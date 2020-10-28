65f99ba82f78c95a7999bb01bb60cb163d88dab6
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-28 19:20:26 +0800

    [TodoList][Baron-JC]refactor: added List and Cards from antd

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { List, Card } from 'antd';
-
 class DoneList extends Component {
+
   render() {
-    const doneTodos = this.props.doneTodos.map((todo) => 
-    <li key={todo.id}>{todo.text} </li>);
+    const doneTodos = this.props.doneTodos.map((todo) =>
+      <Card key={todo.id}>{todo.text} </Card>);
     return (
       <div>
         <h1 id="appName">TodoList</h1>
         <h2 id="appName">Done Items</h2>
-          {doneTodos}
+        <List
+          grid={{ gutter: 16, column: 4 }}
+          dataSource={doneTodos}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
       </div>
     );
   }
