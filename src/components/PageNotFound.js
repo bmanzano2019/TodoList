cb4c7ee765cf5df076a98c3b0e7c5f893fac4715
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-28 22:20:15 +0800

    [TodoList][Baron-JC]refactor: modified the PageNotFound

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,12 +4,14 @@ import React, { Component } from 'react';
 class PageNotFound extends Component {
   render() {
     return (
-      <Result
-        status="404"
-        title="404"
-        subTitle="Sorry, the page you visited does not exist."
-        extra={<Button type="primary" href="/">Back Home</Button>}
-      />
+      <div id="pageNotFound">
+        <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={<Button type="primary" href="/">Back to Home</Button>}
+        />
+      </div>
     );
   }
 }
