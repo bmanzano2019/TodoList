c81ce8a61bf4715ba5785a53557a317308c8cff9
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-28 12:03:05 +0800

    [TodoList][Baron-JC]feature: used react-router-dom APIs like BrowserRouter, Link and Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import {HashRouter,Route,Link,Switch, BrowserRouter} from 'react-router-dom';
+import DoneListContainer from './containers/DoneListContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoList/>
       </header>
+      <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home Page</Link>
+        </li>
+        <li>
+          <Link to="/done">Done Page</Link>
+        </li>
+      </ul>
+        <Route exact path="/" component={TodoList}></Route>
+        <Route path="/done" component={DoneListContainer}></Route>
+      </BrowserRouter>
     </div>
   );
 }
