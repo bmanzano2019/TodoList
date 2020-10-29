64f3dacadfa321b8cec10461b150a92efccbc8f9
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-29 19:01:33 +0800

    [ToDoList][Janelle-JC] refactor : UI changes for DoneList page

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
 
 import { Table } from 'antd';
+import { getTodos } from '../api/todos';
+import { initTodos } from '../actions';
 
 class DoneList extends Component {
 
+  componentDidMount() {
+    getTodos().then(response => {
+      initTodos(response.data);
+    })
+  }
   render() {
-
     const columns = [
       {
         title: 'Done',
         dataIndex: 'text',
         key: 'id',
         render: text => <b>{text}</b>,
-      },
-      {
-        title: 'Date Created',
-        dataIndex: 'createdAt',
-        key: 'id',
-        render: text => <b>{text}</b>,
-        width : 300
       }
     ];
 
     return (
       <div>
-        <h1 id="appName">TodoList</h1>
+        <h1 id="appName">Done List</h1>
         <div className="padding-100">
-        <Table 
-          columns={columns}
-          dataSource={this.props.doneTodos}
-          pagination={{ pageSize: 10 }}          
-        />
+          <Table
+            columns={columns}
+            dataSource={this.props.doneTodos}
+            pagination={{ pageSize: 10 }}
+            
+          />
         </div>
       </div>
     );
