8a8e7afd074ef0d25c05957be23c69616c231bcc
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-29 19:01:06 +0800

    [ToDoList][Janelle-JC] refactor : removed all api/

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,17 +1,18 @@
 import api from './api';
 
 export const getTodos = () => {
-    return api.get('/api/todos')
+    return api.get('/todos')
 }
 
 export const addTodos = (text) => {
-    return api.post('/api/todos', {text})
+    return api.post('/todos', {text})
 }
 
-export const changeStatus = (id, done) => {
-    return api.put(`/api/todos/${id}`, {done: !done})
+export const changeStatus = (id, status) => {
+    return api.put(`/todos/${id}`, {status: !status})
 }
 
 export const deleteTodo = (id) => {
-    return api.delete(`/api/todos/${id}`)
+    return api.delete(`/todos/${id}`)
 }
+
